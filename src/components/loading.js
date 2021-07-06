cf15bba1fb15b6a2a76865253047d2d9885d670e
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-07-06 12:00:12 +0100

    feat: loader component added

---
+++ src/components/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='loader top-2/4 fixed left-1/2'>
+      <h1>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
