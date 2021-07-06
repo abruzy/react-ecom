fc0cd997d154e8f8273360dda4d9e07770783bb9
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-07-06 12:33:15 +0100

    feat: use the Card component in the landing page to make it resuable component

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+import Card from '../components/card'
+
 const LandingPage = () => {
   const [mealsData, setMealsData] = useState([])
 
@@ -27,40 +29,14 @@ const LandingPage = () => {
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-y-5 gap-x-6 px-5'>
         {mealsData.map(
           ({ id, strMeal, strMealThumb, description, title, price }) => (
-            <div
+            <Card
               key={id}
-              className='shadow-sm rounded hover:shadow-xl border border-gray-200 cursor-pointer rounded-bl-3xl rounded-tr-3xl'
-            >
-              <div className='h-64 rounded-tr-3xl rounded-bl-3xl'>
-                <img
-                  src={strMealThumb}
-                  alt=''
-                  className='h-64 object-cover max-w-full w-full rounded-tr-3xl rounded-bl-3xl'
-                />
-              </div>
-              <div>
-                <div className='flex justify-between mt-8 mb-4 px-4'>
-                  <h2 className='text-black1 font-semibold text-2xl'>
-                    {title}
-                  </h2>
-                  <h2 className='text-black1 font-semibold text-2xl'>
-                    {`${price.slice(1, 3)}$`}
-                  </h2>
-                </div>
-                <p className='pl-4 mb-6 text-light-gray text-sm font-medium'>
-                  {strMeal}
-                </p>
-                <p className='px-4 text-light-gray text-sm font-medium'>
-                  {description.slice(0, 100)}
-                </p>
-                <div className='h-20 flex justify-between items-center mt-4'>
-                  <p className='pl-7'>****</p>
-                  <button className='bg-orange1 h-20 px-4 w-28 text-white text-5xl rounded-tl-3xl'>
-                    +
-                  </button>
-                </div>
-              </div>
-            </div>
+              image={strMealThumb}
+              title={title}
+              details={description.slice(0, 100)}
+              price={`${price.slice(1, 3)}$`}
+              figcaption={strMeal}
+            />
           )
         )}
       </div>
